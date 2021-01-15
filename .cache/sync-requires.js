764cf0a35918fc90243008d9908e5860b13cc857
trangrei <trang.lt157@gmail.com>
2021-01-14 21:30:27 -0800

    deploy sitE

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Trang/trang.io/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/Trang/trang.io/src/pages/index.js")))
 }
 
